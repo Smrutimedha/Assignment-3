a054b3c5e1d1d6f14dada2ee8e007ea5c0f00a00
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-09-04 15:26:32 +0530

    Update lab3_question4.cpp

--- lab3_question4.cpp
+++ lab3_question4.cpp
@@ -11,8 +11,8 @@ cout << a <<" is divisible by both 5 and 11\n";
 else if(a%5==0)
 cout << a <<" is divisible by 5\n";
 else if(a%11==0)
-cout << a <<"is divisible by 11\n";
+cout << a <<" is divisible by 11\n";
 else
-cout << a <<"is not divisible by 5 or 11\n";
+cout << a <<" is not divisible by 5 or 11\n";
 return 0;
 }
