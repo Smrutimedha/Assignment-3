e4660de195f49d835ca4112e7ca6f22abbd1ec37
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-09-04 15:13:38 +0530

    Create lab3_question4.cpp

---
+++ lab3_question4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+int a;
+cout << "enter a number\n";
+cin >> a;
+if(a%5==0 && a%11==0)
+cout << a <<" is divisible by both 5 and 11\n";
+else if(a%5==0)
+cout << a <<" is divisible by 5\n";
+else if(a%11==0)
+cout << a <<"is divisible by 11\n";
+else
+cout << a <<"is not divisible by 5 or 11\n";
+return 0;
+}
