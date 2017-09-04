d2138382e9edcca954693427ed6e644e080221c4
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-09-04 15:21:03 +0530

    Create lab3_question6.cpp

---
+++ lab3_question6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+int a;
+cout << "enter a year\n";
+cin >> a;
+if(a%4==0)
+cout << a <<" is a leap year\n";
+else
+cout << a <<"is a general year\n";
+return 0;
+}
