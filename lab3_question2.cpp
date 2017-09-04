223e6d0490282d3575da350a463a7fa71c060691
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-09-04 14:59:37 +0530

    Create lab3_question2.cpp

---
+++ lab3_question2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int a,b,c;
+cout << "enter three numbers\n";
+cin >> a >> b >> c;
+if(a>=b && a>=c)
+cout << a <<" is maximum\n";
+else if(b>=a && b>=c)
+cout << b <<" is maximum\n";
+else
+cout << c <<"is maximum\n";
+return 0;
+}
