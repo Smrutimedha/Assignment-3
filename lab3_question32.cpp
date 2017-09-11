115f924f6e4fe44d8ceec56e173ca74ec3c0d866
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-09-12 02:47:59 +0530

    Create lab3_question32.cpp

---
+++ lab3_question32.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n,first,last;
+    cout << " Input any number: \n";
+    cin >> n;
+    first = n;
+	last=n % 10;
+	for(first=n;first>=10;first=first/10);
+    cout<<" The first digit of "<<n<<" is: "<<first<<endl;
+    cout<<" The last digit of "<<n<<" is: "<<last<<endl;
+}
