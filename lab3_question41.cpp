8087309172ce99c48c7c19778d4eb18bc498cac6
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-09-12 19:59:06 +0530

    Create lab3_question41.cpp

---
+++ lab3_question41.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a;
+    for (int i = 0; i<255; i++)
+    {
+        a = (char) i;
+        cout<<a<<", ";
+    }
+    return 0;
+ 
+}
